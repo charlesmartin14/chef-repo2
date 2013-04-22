10ffa8d25c9d811adaffa845c729e78d4af455a4
Charles H Martin <charlesmartin14@gmail.com>
2013-04-22 14:11:14 -0700

    something is very broken...i can not figure out what

--- cookbooks/cc_worker/recipes/default.rb
+++ cookbooks/cc_worker/recipes/default.rb
@@ -48,7 +48,7 @@ puts "starting worker using #{final_address} as the queue server"
 
 execute "runCrawler" do
     command "cd /home/ubuntu/apps/cloud-crawler;
-    bundle exec /home/ubuntu/apps/cloud-crawler/bin/start_batch_crawl.rb -h #{final_address}" 
+    bundle exec /home/ubuntu/apps/cloud-crawler/bin/run_worker.rb -h #{final_address}" 
     action :run
 end
 
--- roles/cc_master.rb
+++ roles/cc_master.rb
@@ -1,4 +1,4 @@
 name "cc_master"
 description "Master Node that runs qless and redis-bloomfilter"
-run_list "recipe[apt]","recipe[build-essential]","recipe[git]","recipe[cc_master]","recipe[chef-client]"
+run_list "recipe[apt]","recipe[build-essential]","recipe[git]","recipe[ruby_193]","recipe[s3cmd]","recipe[redisio]","recipe[cc_master]","recipe[chef-client]"
 

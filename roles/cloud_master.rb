1fcc4fc38bd6602613d21b6c965260e401533221
Charles H Martin <charlesmartin14@gmail.com>
2013-04-22 17:00:09 -0700

    tried to consolidate roles and recipees

--- roles/cloud_master.rb
+++ roles/cloud_master.rb
@@ -1,4 +1,4 @@
 name "cloud_master"
 description "Master Node that runs qless and redis-bloomfilter"
-run_list "recipe[apt]","recipe[build-essential]","recipe[git]","recipe[ruby_193]","recipe[s3cmd]","recipe[redisio]","recipe[cloud_crawler::master]","recipe[chef-client]"
+run_list "recipe[apt]","recipe[build-essential]","recipe[git]","recipe[ruby_193]","recipe[s3cmd]","recipe[redisio]","recipe[sinatra_app]","recipe[cloud_crawler::master]","recipe[chef-client]"
 

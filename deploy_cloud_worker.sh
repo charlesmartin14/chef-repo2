6630361a65825085059d671b362b02652d427762
Charles H Martin <charlesmartin14@gmail.com>
2013-04-22 17:28:43 -0700

    stll cleaning this up

--- deploy_cloud_worker.sh
+++ deploy_cloud_worker.sh
@@ -3,6 +3,6 @@
 AMI_ID="ami-d70c2892"  #"ami-137bcf7a"
 INSTANCE_TYPE="t1.micro"
 SECURITY_GROUPS="chefami"
-RUNLIST="role[cc_worker]"
+RUNLIST="role[cloud_worker]"
 
 knife ec2 server create -x ubuntu -I $AMI_ID  -f $INSTANCE_TYPE -G $SECURITY_GROUPS -r $RUNLIST

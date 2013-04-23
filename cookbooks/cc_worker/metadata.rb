31f6012a398987d75e7942379f9d31fe6adcf363
Charles H Martin <charlesmartin14@gmail.com>
2013-04-22 17:31:21 -0700

    removed old cc_worker now all in 1 place

--- cookbooks/cc_worker/metadata.rb
+++
@@ -1,9 +0,0 @@
-name             'cc_worker'
-maintainer       'Calculated Content'
-maintainer_email 'charlesmartin14@gmail.com'
-license          'All rights reserved'
-description      'Installs/Configures worker'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
-depends          "redisio"
-depends          "s3cmd"
